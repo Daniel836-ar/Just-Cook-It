08a969aa57865041c15f741493a9c8c730202382
Oluf-Snejni <deripalovrostislav@gmail.com>
2025-11-25 20:04:44 +0700

    обавил больше ингрдиентов + разсортировал по категориям + в алфавитном порядке

--- src/main/java/org/example/service/Main.java
+++ src/main/java/org/example/service/Main.java
@@ -30,8 +30,52 @@ public class Main implements CommandLineRunner {
     public void run(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // получение ингредиентов (ну в данном случае их создание , но по логике получение)
-        Ingredient ingredient = ingredientService.findOrCreateIngredient("Яйцо");
-        Ingredient ingredient2 = ingredientService.findOrCreateIngredient("Яблоко");
+
+        //овощи
+        Ingredient ingredient_potato = ingredientService.findOrCreateIngredient("Картофель");
+        Ingredient ingredient_onion = ingredientService.findOrCreateIngredient("Лук Репчатый");
+        Ingredient ingredient_carrot = ingredientService.findOrCreateIngredient("Морковь");
+        Ingredient ingredient_cucumber = ingredientService.findOrCreateIngredient("Огурец");
+        Ingredient ingredient_BulgarianPepper = ingredientService.findOrCreateIngredient("Перец Болгарский");
+        Ingredient ingredient_tomato = ingredientService.findOrCreateIngredient("Помидор");
+        Ingredient ingredient_beet = ingredientService.findOrCreateIngredient("Свёкла");
+        Ingredient ingredient_garlic = ingredientService.findOrCreateIngredient("Чеснок");
+
+        //фрукты
+        Ingredient ingredient_pineapple = ingredientService.findOrCreateIngredient("Ананас");
+        Ingredient ingredient_orange = ingredientService.findOrCreateIngredient("Апельсин");
+        Ingredient ingredient_banana = ingredientService.findOrCreateIngredient("Банан");
+        Ingredient ingredient_grapes = ingredientService.findOrCreateIngredient("Виноград");
+        Ingredient ingredient_paper = ingredientService.findOrCreateIngredient("Груша");
+        Ingredient ingredient_lime = ingredientService.findOrCreateIngredient("Лайм");
+        Ingredient ingredient_lemon = ingredientService.findOrCreateIngredient("Лимон");
+        Ingredient ingredient_apple = ingredientService.findOrCreateIngredient("Яблоко");
+
+        //Молочка
+        Ingredient ingredient_yogurt = ingredientService.findOrCreateIngredient("Йогурт");
+        Ingredient ingredient_kefir = ingredientService.findOrCreateIngredient("Кефир");
+        Ingredient ingredient_margarine = ingredientService.findOrCreateIngredient("Маргарин");
+        Ingredient ingredient_better = ingredientService.findOrCreateIngredient("Масло");
+        Ingredient ingredient_milk = ingredientService.findOrCreateIngredient("Молоко");
+        Ingredient ingredient_cream = ingredientService.findOrCreateIngredient("Сливки");
+        Ingredient ingredient_sourCream = ingredientService.findOrCreateIngredient("Сметана");
+        Ingredient ingredient_cheese = ingredientService.findOrCreateIngredient("Сыр");
+        Ingredient ingredient_curd = ingredientService.findOrCreateIngredient("Творог");
+
+        //мука
+        Ingredient ingredient_amaranthFlour = ingredientService.findOrCreateIngredient("Мука Амарантовая");
+        Ingredient ingredient_riceFlour = ingredientService.findOrCreateIngredient("Мука Кукурузная");
+        Ingredient ingredient_wheatFlour = ingredientService.findOrCreateIngredient("Мука Пшеничная");
+        Ingredient ingredient_cornFlour = ingredientService.findOrCreateIngredient("Мука Рисовая");
+
+        //Мясо/Яйца
+        Ingredient ingredient_beef = ingredientService.findOrCreateIngredient("Говядина");
+        Ingredient ingredient_turkey = ingredientService.findOrCreateIngredient("Индейка");
+        Ingredient ingredient_chicken = ingredientService.findOrCreateIngredient("Курица");
+        Ingredient ingredient_pork = ingredientService.findOrCreateIngredient("Свинина");
+        Ingredient ingredient_sausages = ingredientService.findOrCreateIngredient("Сосиски");
+        Ingredient ingredient_chickenEgg = ingredientService.findOrCreateIngredient("Яйцо Куринное");
+        Ingredient ingredient_quailEgg = ingredientService.findOrCreateIngredient("Яйцо Перепелиное");
 
 
         // Тестовые листы количества ингредиентов
